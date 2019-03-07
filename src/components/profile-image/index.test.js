bbf3fdc09e4b51ddbf9297b25396955a6922ee04
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-03-07 13:19:07 +0300

    test added for profile-image

---
+++ src/components/profile-image/index.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ProfileImage from './index';
+import renderer from 'react-test-renderer';
+
+import wrapWithIntl from '../../utils/test-helper';
+
+it('not in edit mode and image is empty. should render empty image svg ', () => {
+  const props = {};
+
+  const component = wrapWithIntl(<ProfileImage {...props} />);
+
+  const tree = renderer
+    .create(component)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('in edit mode and image is empty. should render empty image svg and edit btn', () => {
+  const props = {
+    editMode: true,
+    ui: {
+      skipAccountDialog: false
+    }
+  };
+
+  const component = wrapWithIntl(<ProfileImage {...props} />);
+
+  const tree = renderer
+    .create(component)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('not in edit mode and image is not empty. should render image ', () => {
+  const props = {
+    image: [{
+      contentUrl: 'https://foo.bar/baz.jpg'
+    }]
+  };
+
+  const component = wrapWithIntl(<ProfileImage {...props} />);
+
+  const tree = renderer
+    .create(component)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('in edit mode and image is not empty. should render image and edit btn', () => {
+  const props = {
+    editMode: true,
+    ui: {
+      skipAccountDialog: false
+    },
+    image: [{
+      contentUrl: 'https://foo.bar/baz.jpg'
+    }]
+  };
+
+  const component = wrapWithIntl(<ProfileImage {...props} />);
+
+  const tree = renderer
+    .create(component)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
