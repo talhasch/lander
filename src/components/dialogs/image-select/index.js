25d2a7f451c929550fe9f92336bab5e1c57aff85
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-03-09 17:52:09 +0300

    licence note added to image select dialog

--- src/components/dialogs/image-select/index.js
+++ src/components/dialogs/image-select/index.js
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types';
 
 import {Modal} from 'react-bootstrap';
 
+import {FormattedMessage, FormattedHTMLMessage} from 'react-intl';
+
 import images from '../../../data/bg-images'
 
 class ImageSelectDialog extends Component {
-
 
   cancel = () => {
     const {onCancel} = this.props;
@@ -25,15 +26,15 @@ class ImageSelectDialog extends Component {
   render() {
     const {onCancel} = this.props;
 
-
     return (
       <>
-        <Modal show onHide={onCancel} size="lg">
+        <Modal show onHide={onCancel} size="lg" className="image-select-dialog">
           <Modal.Header closeButton>
-
+            <Modal.Title><FormattedMessage id="image-select.title" /></Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <div className="image-select-dialog-content">
+              <p className="licence-note"><FormattedHTMLMessage id="image-select.licence-note" /></p>
               <div className="image-list">
                 {images.map(i => <div className="image-item" key={i.name} style={{backgroundImage: `url(${i.url})`}}
                                       onClick={() => {
