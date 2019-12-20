560d930ac7c3432a0294e93ec7d2a8ec6722ce26
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-12-20 15:59:30 +0300

    cant be evil

--- server/server.js
+++ server/server.js
@@ -29,6 +29,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("can't-be-evil", 'true');
   next();
 });
 
